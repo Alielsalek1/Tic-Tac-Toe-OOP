771e15e979962244420a1db10aced11edc5343b6
Mohamed-Amr7 <moh.amr777@gmail.com>
2023-12-15 18:37:00 +0200

    Constructed the random player with appropriate board dimensions

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ int main() {
     cout << "Press 1 if you want to play with computer: ";
     cin >> choice;
     if (choice != 1) players[1] = new Player (2, 'o');
-    else players[1] = new RandomPlayer ('o', 3);    //Player pointer points to child
 
     cout << "choose a type of game to play from 1-3 :\n"
             "1 - 3x3\n"
@@ -20,22 +19,26 @@ int main() {
             "4 - four in a row\n";
     int c; cin >> c;
     if (c == 1) {
+        if(choice == 1) players[1] = new RandomPlayer ('o', 3);
         GameManager x_o_game (new X_O_Board(), players);
         x_o_game.run();
         system ("pause");
     } else if (c == 2) {
+        if(choice == 1) players[1] = new RandomPlayer ('o', 5);
         GameManager x_o_game(new pyramid_board(), players);
         x_o_game.run();
         system ("pause");
     } else if (c == 3){
         Player *tmp[2] = {players[0], players[1]};
         players[0] = new fiveByFivePlayer('x', players[0]);
-        players[1] = new fiveByFivePlayer('o', players[1]);
+        if(choice == 1) players[1] = new RandomPlayer ('o', 3);
+        else players[1] = new fiveByFivePlayer('o', players[1]);
         for (auto & player : tmp) delete player;
         GameManager x_o_game(new five_by_five(players), players);
         x_o_game.run();
         system ("pause");
     } else {
+        if(choice == 1) players[1] = new RandomPlayer ('o', 7);
         GameManager x_o_game(new four_in_a_row(), players);
         x_o_game.run();
         system ("pause");
